2f3b39c5ef5ee52372cd7c0caef07bc186f17790
Martyn West <657393+westy@users.noreply.github.com>
2022-03-15 17:36:14 +0000

    Moved my exercising files out into another project, and moved package content slightly

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,7 @@ import sys, os, time
 #sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath('..'))
 
-from src.pyseext.Ext import Ext
+from pyseext.Ext import Ext
 
 # -- Project information -----------------------------------------------------
 
