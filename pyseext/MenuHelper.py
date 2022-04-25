4341fa902b5f23ed8726cf6abbc40da5828ea968
westy <657393+westy@users.noreply.github.com>
2022-04-25 17:11:38 +0100

    Added methods to menu helper to allow checking for enabled/disabled items

--- pyseext/MenuHelper.py
+++ pyseext/MenuHelper.py
@@ -7,7 +7,8 @@ class MenuHelper:
     """
 
     # Class variables
-    _MENU_ITEM_TEMPLATE = 'menuitem[text="{text}"][disabled=false]'
+    _ENABLED_MENU_ITEM_TEMPLATE = 'menuitem[text="{text}"][disabled=false]'
+    _DISABLED_MENU_ITEM_TEMPLATE = 'menuitem[text="{text}"][disabled=true]'
 
     def __init__(self, driver):
         """Initialises an instance of this class
@@ -28,9 +29,29 @@ class MenuHelper:
             root_id (str, optional): The id of the container within which to perform the query.
                 If omitted, all components within the document are included in the search.
         """
-        menu_item = self._cq.wait_for_single_query_visible(self._MENU_ITEM_TEMPLATE.format(text=text), root_id)
+        menu_item = self._cq.wait_for_single_query_visible(self._ENABLED_MENU_ITEM_TEMPLATE.format(text=text), root_id)
 
         # Rather than call click, move mouse to the menu item and click...
         self._action_chains.move_to_element(menu_item)
         self._action_chains.click()
         self._action_chains.perform()
+
+    def check_menu_item_enabled(self, text, root_id=None):
+        """Checks that we can find an enabled menu item with the specified text.
+
+        Args:
+            text (str): The text on the menu item
+            root_id (str, optional): The id of the container within which to perform the query.
+                If omitted, all components within the document are included in the search.
+        """
+        self._cq.wait_for_single_query(self._ENABLED_MENU_ITEM_TEMPLATE.format(text=text), root_id)
+
+    def check_menu_item_disabled(self, text, root_id=None):
+        """Checks that we can find a disabled menu item with the specified text.
+
+        Args:
+            text (str): The text on the menu item
+            root_id (str, optional): The id of the container within which to perform the query.
+                If omitted, all components within the document are included in the search.
+        """
+        self._cq.wait_for_single_query(self._DISABLED_MENU_ITEM_TEMPLATE.format(text=text), root_id)
