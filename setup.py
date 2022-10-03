c08d1f9680adc4909535683445a12daf0e27f7c5
Martyn West <657393+westy@users.noreply.github.com>
2022-10-03 14:53:33 +0100

    Another version update test!

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open('LICENSE', encoding = 'utf-8') as f:
 
 setup(
     name='pyseext',
-    version='1.0.1',
+    version='1.0.2',
     description='Python Selenium ExtJS - package for helping interact with an ExtJS application from Python using Selenium',
     long_description=readme,
     author='Martyn West',
